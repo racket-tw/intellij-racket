ae878421ed06425c0efab92d2c320b323754c192
dannypsnl <dannypsnl@gmail.com>
2020-06-06 23:57:17 +0800

    remove keyword highlighting, but introduce PARENTHESES

--- src/main/java/tw/racket/lang/RacketSyntaxHighlighter.java
+++ src/main/java/tw/racket/lang/RacketSyntaxHighlighter.java
@@ -29,33 +29,37 @@ public class RacketSyntaxHighlighter extends SyntaxHighlighterBase {
             return NUMBER_KEYS;
         } else if (tokenType.equals(RacketTypes.CHAR_LITERAL)) {
             return STRING_KEYS;
-        } else if (tokenType.equals(RacketTypes.KEYWORD)) {
-            return KEYWORD_KEYS;
         } else if (tokenType.equals(RacketTypes.COMMENT)) {
             return COMMENT_KEYS;
         } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
             return BAD_CHAR_KEYS;
+        } else if (tokenType.equals(RacketTypes.LPAREN) || tokenType.equals(RacketTypes.RPAREN)) {
+            return PARENTHESES_KEYS;
+        } else if (tokenType.equals(RacketTypes.LBRACE) || tokenType.equals(RacketTypes.RBRACE)) {
+            return BRACES_KEYS;
         } else {
             return EMPTY_KEYS;
         }
     }
 
+    public static final TextAttributesKey BRACES =
+            createTextAttributesKey("RACKET_BRACES", DefaultLanguageHighlighterColors.BRACES);
+    public static final TextAttributesKey PARENTHESES =
+            createTextAttributesKey("RACKET_PARENTHESES", DefaultLanguageHighlighterColors.PARENTHESES);
     public static final TextAttributesKey STRING =
             createTextAttributesKey("RACKET_STRING", DefaultLanguageHighlighterColors.STRING);
     public static final TextAttributesKey NUMBER =
             createTextAttributesKey("RACKET_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
-    public static final TextAttributesKey KEYWORD =
-            createTextAttributesKey("RACKET_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
     public static final TextAttributesKey COMMENT =
             createTextAttributesKey("RACKET_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
     public static final TextAttributesKey BAD_CHARACTER =
             createTextAttributesKey("RACKET_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
-
+    private static final TextAttributesKey[] BRACES_KEYS = new TextAttributesKey[]{BRACES};
+    private static final TextAttributesKey[] PARENTHESES_KEYS = new TextAttributesKey[]{PARENTHESES};
     private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
     private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
     private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-    private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD};
     private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
     private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 }
