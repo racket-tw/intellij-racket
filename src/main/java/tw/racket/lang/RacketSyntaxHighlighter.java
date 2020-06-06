636a6dd3232146c7eb4b38d893be92eee7e223f1
dannypsnl <dannypsnl@gmail.com>
2020-06-06 23:33:16 +0800

    support string char and number hightlighting

--- src/main/java/tw/racket/lang/RacketSyntaxHighlighter.java
+++ src/main/java/tw/racket/lang/RacketSyntaxHighlighter.java
@@ -24,6 +24,12 @@ public class RacketSyntaxHighlighter extends SyntaxHighlighterBase {
     @Override
     public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
         if (tokenType.equals(RacketTypes.STRING_LITERAL)) {
+            return STRING_KEYS;
+        } else if (tokenType.equals(RacketTypes.NUMBER_LITERAL)) {
+            return NUMBER_KEYS;
+        } else if (tokenType.equals(RacketTypes.CHAR_LITERAL)) {
+            return STRING_KEYS;
+        } else if (tokenType.equals(RacketTypes.KEYWORD)) {
             return KEYWORD_KEYS;
         } else if (tokenType.equals(RacketTypes.COMMENT)) {
             return COMMENT_KEYS;
@@ -34,6 +40,10 @@ public class RacketSyntaxHighlighter extends SyntaxHighlighterBase {
         }
     }
 
+    public static final TextAttributesKey STRING =
+            createTextAttributesKey("RACKET_STRING", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey NUMBER =
+            createTextAttributesKey("RACKET_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
     public static final TextAttributesKey KEYWORD =
             createTextAttributesKey("RACKET_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
     public static final TextAttributesKey COMMENT =
@@ -42,6 +52,8 @@ public class RacketSyntaxHighlighter extends SyntaxHighlighterBase {
             createTextAttributesKey("RACKET_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
 
+    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
+    private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
     private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
     private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD};
     private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
