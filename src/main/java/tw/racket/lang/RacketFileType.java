a09ebda43819c879cb0bd9f699c86d63b7ccfe15
dannypsnl <dannypsnl@gmail.com>
2020-06-18 21:46:10 +0800

    (#12) provide icon
    resolve #12

--- src/main/java/tw/racket/lang/RacketFileType.java
+++ src/main/java/tw/racket/lang/RacketFileType.java
@@ -34,6 +34,6 @@ public class RacketFileType extends LanguageFileType {
     @Nullable
     @Override
     public Icon getIcon() {
-        return null;
+        return RacketIcons.FILE;
     }
 }
---
+++ src/main/java/tw/racket/lang/RacketIcons.java
@@ -0,0 +1,10 @@
+package tw.racket.lang;
+
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class RacketIcons {
+    public static final Icon FILE = IconLoader.getIcon("/icons/racket-icon.png");
+}
